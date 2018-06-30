b05d9b4feac1ea976c34be8fc10e0cfa48221800
Maria <maria.eckstein@berkeley.edu>
2018-06-29 18:14:59 -0700

    update_Q and p_from_Q

--- models/HierAgents2.py
+++ models/HierAgents2.py
@@ -10,31 +10,28 @@ import matplotlib.pyplot as plt
 
 
 # Step 1: Define update_Q in Theano scan to avoid the loop over trials
-from update_Q import *
+from update_Q import update_Q, p_from_Q
 
 # Get agent data (choices, rewards)
 n_trials = 5
-agent_data = pd.read_csv(glob.glob('C:/Users/maria/MEGAsync/SLCN/PSGenRec/*.csv')[0])
-left_choices = np.array(agent_data['selected_box'])[:n_trials]
-right_choices = 1 - np.array(agent_data['selected_box'])[:n_trials]
-rewards = agent_data['reward'].tolist()[:n_trials]
 alpha = 0.5
+beta = 3
 
-print(choices)
-print(rewards)
-print(update_Q(rewards, right_choices, alpha))
+agent_data = pd.read_csv(glob.glob('C:/Users/maria/MEGAsync/SLCN/PSGenRec/*.csv')[0])
+left_choices = 1 - np.array(agent_data['selected_box'])[:n_trials]
+right_choices = np.array(agent_data['selected_box'])[:n_trials]
+rewards = agent_data['reward'].tolist()[:n_trials]
+
+Q_left = update_Q(rewards, left_choices, alpha).T[0]
+Q_right = update_Q(rewards, right_choices, alpha).T[0]
+
+p_left = p_from_Q(Q_left, Q_right, beta)
+p_right = p_from_Q(Q_right, Q_left, beta)
+
+print(np.column_stack((Q_left, Q_right, p_left, p_right, rewards, left_choices)))
 
 
-Q = T.vector('Q')
-beta = T.scalar('beta')
 
-def p_from_Q_(Q, beta):
-    return 1 / (1 + T.exp(-beta * (Q[1] - Q[0])))
-
-output, updates = theano.scan(fn=p_from_Q_,
-                              sequences=[Q],
-                              non_sequences=[beta])
-#
 #
 # with pm.Model() as model:
 #
--- models/update_Q.py
+++ models/update_Q.py
@@ -5,6 +5,7 @@ import theano
 import theano.tensor as T
 
 
+# Function 1: update_Q
 # Data types of the inputs for update_Q
 rewards = T.vector('rewards')
 choices = T.vector('choices')
@@ -21,8 +22,8 @@ def update_Q_(reward, choice, Q_old, alpha):
 # Get scan output and updates for update_Q, telling it how the loop will be performed
 output, updates = theano.scan(fn=update_Q_,  # function that will executed in each iteration, with the following inputs:
                               sequences=[rewards, choices],  # parameter with a different value in each iteration
-                              outputs_info=[Q_old],  # parameter that will be updated in each iteration
-                              non_sequences=[alpha])  # parameter with the same value across iterations
+                              outputs_info=Q_old,  # parameter that will be updated in each iteration
+                              non_sequences=alpha)  # parameter with the same value across iterations
 
 # Make update_Q a scan function, encompassing the loop
 update_Q = theano.function(inputs=[rewards, choices, alpha],
@@ -37,6 +38,37 @@ update_Q = theano.function(inputs=[rewards, choices, alpha],
 # rewards = agent_data['reward'].tolist()[:n_trials]
 # alpha = 0.5
 #
-# print(choices)
+# print(right_choices)
 # print(rewards)
 # print(update_Q(rewards, right_choices, alpha))
+
+
+# Function 2: p_from_Q
+# Define data types for parameters
+Q_left = T.vector('Q_left')
+Q_right = T.vector('Q_right')
+beta = T.scalar('beta')
+
+
+def p_from_Q_(Q_left, Q_right, beta):
+    return 1 / (1 + T.exp(-beta * (Q_left - Q_right)))
+
+
+output, updates = theano.scan(fn=p_from_Q_,
+                              sequences=[Q_left, Q_right],
+                              non_sequences=[beta])
+
+p_from_Q = theano.function(inputs=[Q_left, Q_right, beta],
+                           outputs=output,
+                           updates=updates)
+# # Test on data
+# n_trials = 5
+# agent_data = pd.read_csv(glob.glob('C:/Users/maria/MEGAsync/SLCN/PSGenRec/*.csv')[0])
+# left_choices = 1 - np.array(agent_data['selected_box'])[:n_trials]
+# right_choices = np.array(agent_data['selected_box'])[:n_trials]
+# Q_left = update_Q(rewards, left_choices, alpha).T[0]
+# Q_right = update_Q(rewards, right_choices, alpha).T[0]
+# beta = 3
+#
+# print(np.column_stack((Q_left, Q_right, rewards, left_choices)))
+# print(p_from_Q(Q_left, Q_right, beta))
