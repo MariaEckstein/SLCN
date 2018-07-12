849978695e61018544bc1ae7d9864423aca626f8
Maria <maria.eckstein@berkeley.edu>
2018-07-11 23:25:00 -0700

    add option for kids and teens only

--- models/HierarchicalModeling.py
+++ models/HierarchicalModeling.py
@@ -26,9 +26,11 @@ compare_RL_Bayes = True
 
 # Which data should be fitted?
 fitted_data_name = 'humans'  # 'humans', 'simulations'
+kids_and_teens_only = True
+adults_only = False
 
 # Sampling details
-n_samples = 10000
+n_samples = 5000
 n_tune = 200
 n_chains = 2
 
@@ -71,6 +73,16 @@ for file_idx, filename in enumerate(filenames):
 rewards = np.delete(rewards, range(file_idx+1, n_subj), 1)
 choices = np.delete(choices, range(file_idx+1, n_subj), 1)
 ages = ages[:file_idx+1]
+
+if kids_and_teens_only:
+    rewards = rewards[:, ages <= 18]
+    choices = choices[:, ages <= 18]
+    ages = ages[ages <= 18]
+elif adults_only:
+    rewards = rewards[:, ages > 18]
+    choices = choices[:, ages > 18]
+    ages = ages[ages > 18]
+
 n_subj = choices.shape[1]
 
 # Look at data
@@ -90,28 +102,27 @@ with pm.Model() as Bayes_model:
     rewards = T.as_tensor_variable(rewards)
     choices = T.as_tensor_variable(choices)
     ages = T.as_tensor_variable(ages)
-    initial_p = 0.5 * T.ones((1, n_subj))  # get first entry
 
     # Model parameters
     if fitting_method == 'hierarchical':
 
         # Population-level priors (as un-informative as possible)
-        epsilon_mu, epsilon_sd = pm.Uniform('epsilon_mu', lower=0, upper=1), 0.2
+        epsilon_mu, epsilon_sd = pm.Uniform('epsilon_mu', lower=0, upper=1), 0.1
+        epsilon_slope = pm.Uniform('epsilon_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)  #
         p_switch_mu, p_switch_sd = pm.Uniform('p_switch_mu', lower=0, upper=1), 0.1  # pm.HalfNormal('p_switch_sd', sd=0.1)
-        p_switch_slope = T.as_tensor_variable(pm.Uniform('p_switch_slope', lower=-1, upper=1))
+        p_switch_slope = pm.Uniform('p_switch_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)
         p_reward_mu, p_reward_sd = pm.Uniform('p_reward_mu', lower=0, upper=1), 0.1  # pm.HalfNormal('p_reward_sd', sd=0.1)
-        p_reward_slope = T.as_tensor_variable(pm.Uniform('p_reward_slope', lower=-1, upper=1))
+        p_reward_slope = pm.Uniform('p_reward_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)
         # p_noisy_mu, p_noisy_sd = pm.Uniform('p_noisy_mu', lower=0, upper=1), 0.1  # pm.HalfNormal('p_noisy_sd', sd=0.1)
 
         # Individual parameters (bounds avoid initial energy==-inf because logp(RV)==-inf)
-        epsilon = pm.Bound(pm.Normal, lower=0, upper=1)('epsilon', mu=epsilon_mu, sd=epsilon_sd, shape=n_subj)
-        p_switch = pm.Bound(pm.Normal, lower=0, upper=1)('p_switch',
-                                                         mu=p_switch_mu + ages * p_switch_slope, sd=p_switch_sd, shape=n_subj)  #
-        T.printing.Print(p_switch)
-        p_reward = pm.Bound(pm.Normal, lower=0, upper=1)('p_reward',
-                                                         mu=p_reward_mu + ages * p_reward_slope, sd=p_reward_sd, shape=n_subj)  #
-        T.printing.Print(p_reward)
-        p_noisy = 0.09 * T.ones(n_subj)  # pm.Bound(pm.Normal, lower=0, upper=1)('p_noisy', mu=p_noisy_mu, sd=p_noisy_sd, shape=n_subj)
+        epsilon = pm.Bound(pm.Normal, lower=0,
+                           upper=1)('epsilon', mu=epsilon_mu + ages * epsilon_slope, sd=epsilon_sd, shape=n_subj)
+        p_switch = pm.Bound(pm.Normal, lower=0,
+                            upper=1)('p_switch', mu=p_switch_mu + ages * p_switch_slope, sd=p_switch_sd, shape=n_subj)
+        p_reward = pm.Bound(pm.Normal, lower=0,
+                            upper=1)('p_reward', mu=p_reward_mu + ages * p_reward_slope, sd=p_reward_sd, shape=n_subj)
+        p_noisy = 0.01 * T.ones(n_subj)  # pm.Bound(pm.Normal, lower=0, upper=1)('p_noisy', mu=p_noisy_mu, sd=p_noisy_sd, shape=n_subj)
 
     elif fitting_method == 'flat':
 
@@ -131,6 +142,7 @@ with pm.Model() as Bayes_model:
                              outputs_info=[p_right],
                              non_sequences=[p_switch, epsilon])
 
+    initial_p = 0.5 * T.ones((1, n_subj))  # get first entry
     p_right = T.concatenate([initial_p, p_right[:-1]], axis=0)  # add initial p=0.5 at the beginning
 
     # Use Bernoulli to sample responses
@@ -146,7 +158,7 @@ with pm.Model() as Bayes_model:
         for RV in Bayes_model.basic_RVs:
             print("\tlogp of {0}: {1}".format(RV.name, RV.logp(Bayes_model.test_point)))
 
-    # Sample the model (should be >=5000, tune>=500)
+    # Sample the model
     Bayes_trace = pm.sample(n_samples, tune=n_tune, chains=n_chains, cores=1)
 
 # Show results
@@ -157,9 +169,10 @@ Bayes_model_summary = pm.summary(Bayes_trace)
 now = datetime.datetime.now()
 Bayes_model_id = '_'.join([str(now.year), str(now.month), str(now.day), str(now.hour), str(now.minute),
                      fitted_data_name, "Bayes", fitting_method, 'n_samples' + str(n_samples)])
-print('Pickling Bayes_trace and Bayes_model, saving traceplot to {0}{1}...\n'.format(save_dir, Bayes_model_id))
+print('Pickling Bayes trace, model, and model summary, saving traceplot to {0}{1}...\n'.format(save_dir, Bayes_model_id))
 with open(save_dir + Bayes_model_id + '.pickle', 'wb') as handle:
-    pickle.dump({'trace': Bayes_trace, 'model': Bayes_model}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump({'trace': Bayes_trace, 'model': Bayes_model, 'summary': Bayes_model_summary},
+                handle, protocol=pickle.HIGHEST_PROTOCOL)
 plt.savefig(save_dir + 'plot_' + Bayes_model_id + '.png')
 
 with pm.Model() as RL_model:
@@ -168,27 +181,30 @@ with pm.Model() as RL_model:
     rewards = T.as_tensor_variable(rewards)
     choices = T.as_tensor_variable(choices)
     ages = T.as_tensor_variable(ages)
-    initial_p = 0.5 * T.ones((1, n_subj))  # get first entry
 
     # Model parameters
     if fitting_method == 'hierarchical':
 
         # Population-level priors (as un-informative as possible)
-        epsilon_mu, epsilon_sd = pm.Uniform('epsilon_mu', lower=0, upper=1), 0.2
-        epsilon_slope = pm.Uniform('epsilon_slope', lower=-1, upper=1)
+        epsilon_mu, epsilon_sd = pm.Uniform('epsilon_mu', lower=0, upper=1), 0.1
+        epsilon_slope = pm.Uniform('epsilon_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)
         alpha_mu, alpha_sd = pm.Uniform('alpha_mu', lower=0, upper=1), 0.1  # pm.HalfNormal('alpha_sd', sd=0.1)
-        alpha_slope = pm.Uniform('alpha_slope', lower=-1, upper=1)
+        alpha_slope = pm.Uniform('alpha_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)
         calpha_sc_mu, calpha_sc_sd = pm.Uniform('calpha_sc_mu', lower=0, upper=1), 0.1  # pm.HalfNormal('calpha_sc_sd', sd=0.1)
-        calpha_slope = pm.Uniform('calpha_slope', lower=-1, upper=1)
+        calpha_slope = pm.Uniform('calpha_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)
         beta_mu, beta_sd = pm.Lognormal('beta_mu', mu=0, sd=1), 3  # pm.HalfNormal('beta_sd', sd=0.1)
-        beta_slope = pm.Uniform('beta_slope', lower=-1, upper=1)
+        beta_slope = pm.Uniform('beta_slope', lower=-1, upper=1)  # T.as_tensor_variable(0.)  #
 
         # Individual parameters (bounds avoid initial energy==-inf because logp(RV)==-inf)
-        epsilon = pm.Bound(pm.Normal, lower=0, upper=1)('epsilon', mu=epsilon_mu + ages * epsilon_slope, sd=epsilon_sd, shape=n_subj)
-        alpha = pm.Bound(pm.Normal, lower=0, upper=1)('alpha', mu=alpha_mu + ages * alpha_slope, sd=alpha_sd, shape=n_subj)
-        calpha_sc = pm.Bound(pm.Normal, lower=0, upper=1)('calpha_sc', mu=calpha_sc_mu + ages * calpha_slope, sd=alpha_sd, shape=n_subj)
+        epsilon = pm.Bound(pm.Normal, lower=0,
+                           upper=1)('epsilon', mu=epsilon_mu + ages * epsilon_slope, sd=epsilon_sd, shape=n_subj)
+        alpha = pm.Bound(pm.Normal, lower=0,
+                         upper=1)('alpha', mu=alpha_mu + ages * alpha_slope, sd=alpha_sd, shape=n_subj)
+        calpha_sc = pm.Bound(pm.Normal, lower=0,
+                             upper=1)('calpha_sc', mu=calpha_sc_mu + ages * calpha_slope, sd=alpha_sd, shape=n_subj)
+        beta = pm.Bound(pm.Normal,
+                        lower=0)('beta', mu=beta_mu + ages * beta_slope, sd=beta_sd, shape=n_subj)
         calpha = pm.Deterministic('calpha', alpha * calpha_sc)
-        beta = pm.Normal('beta', mu=beta_mu + beta_slope, sd=beta_sd, shape=n_subj)
 
     elif fitting_method == 'flat':
 
@@ -196,8 +212,8 @@ with pm.Model() as RL_model:
         epsilon = pm.Uniform('epsilon', lower=0, upper=1, shape=n_subj)
         alpha = pm.Uniform('alpha', lower=0, upper=1, shape=n_subj)
         calpha_sc = pm.Uniform('calpha_sc', lower=0, upper=1, shape=n_subj)
-        calpha = alpha * calpha_sc
         beta = pm.Lognormal('beta', mu=0, sd=1, shape=n_subj)
+        calpha = alpha * calpha_sc
 
     # Calculate Q-values
     initial_Q_left = 0.5 * T.ones(n_subj)
@@ -210,6 +226,7 @@ with pm.Model() as RL_model:
     # Translate Q-values into probabilities and add epsilon noise
     p_right = shared.p_from_Q(Q_left, Q_right, beta, epsilon)
 
+    initial_p = 0.5 * T.ones((1, n_subj))
     p_right = T.concatenate([initial_p, p_right[:-1]], axis=0)  # add initial p=0.5 at the beginning
 
     # Use Bernoulli to sample responses
@@ -225,7 +242,7 @@ with pm.Model() as RL_model:
         for RV in RL_model.basic_RVs:
             print("\tlogp of {0}: {1}".format(RV.name, RV.logp(RL_model.test_point)))
 
-    # Sample the model (should be >=5000, tune>=500)
+    # Sample the model
     RL_trace = pm.sample(n_samples, tune=n_tune, chains=n_chains, cores=1)
 
 # Show results
@@ -236,65 +253,23 @@ RL_model_summary = pm.summary(RL_trace)
 now = datetime.datetime.now()
 RL_model_id = '_'.join([str(now.year), str(now.month), str(now.day), str(now.hour), str(now.minute),
                      fitted_data_name, "RL", fitting_method, 'n_samples' + str(n_samples)])
-print('Pickling RL_trace and RL_model, saving traceplot to {0}{1}...\n'.format(save_dir, RL_model_id))
+print('Pickling RL trace, model, and summary, saving traceplot to {0}{1}...\n'.format(save_dir, RL_model_id))
 with open(save_dir + RL_model_id + '.pickle', 'wb') as handle:
-    pickle.dump({'trace': RL_trace, 'model': RL_model}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump({'trace': RL_trace, 'model': RL_model, 'summary': RL_model_summary},
+                handle, protocol=pickle.HIGHEST_PROTOCOL)
 plt.savefig(save_dir + 'plot_' + RL_model_id + '.png')
 
 # Compare WAIC scores
 print("Comparing models...")
-RL_model.name = 'RL_model'
-Bayes_model.name = 'Bayes_model'
+RL_model.name = 'RL'
+Bayes_model.name = 'Bayes'
 df_comp_WAIC = pm.compare({RL_model: RL_trace, Bayes_model: Bayes_trace})
 
 pm.compareplot(df_comp_WAIC)
-plt.savefig(save_dir + 'compareplot_WAIC.png')
+plt.savefig(save_dir + 'compareplot_WAIC' + RL_model_id + '.png')
 
 # Compare leave-one-out cross validation
 df_comp_LOO = pm.compare({RL_model: RL_trace, Bayes_model: Bayes_trace}, ic='LOO')
 
 pm.compareplot(df_comp_LOO)
-plt.savefig(save_dir + 'compareplot_LOO.png')
-
-# NOTES
-# Hierarchical fitting example: http://docs.pymc.io/notebooks/multilevel_modeling.html
-# And: http://docs.pymc.io/notebooks/rugby_analytics.html
-# with pm.Model() as model:
-#     # global model parameters
-#     home = pm.Flat('home')
-#     sd_att = pm.HalfStudentT('sd_att', nu=3, sd=2.5)
-#     sd_def = pm.HalfStudentT('sd_def', nu=3, sd=2.5)
-#     intercept = pm.Flat('intercept')
-#
-#     # team-specific model parameters
-#     atts_star = pm.Normal("atts_star", mu=0, sd=sd_att, shape=num_teams)
-#     defs_star = pm.Normal("defs_star", mu=0, sd=sd_def, shape=num_teams)
-#
-#     atts = pm.Deterministic('atts', atts_star - tt.mean(atts_star))
-#     defs = pm.Deterministic('defs', defs_star - tt.mean(defs_star))
-#     home_theta = tt.exp(intercept + home + atts[home_team] + defs[away_team])
-#     away_theta = tt.exp(intercept + atts[away_team] + defs[home_team])
-#
-#     # likelihood of observed data
-#     home_points = pm.Poisson('home_points', mu=home_theta, observed=observed_home_goals)
-#     away_points = pm.Poisson('away_points', mu=away_theta, observed=observed_away_goals)
-
-# Problem with "bad initial energy error":
-# logp(RV) == -inf and/or logp(model) == -inf because Bernoulli can't handle negative probabilities, which can arise
-# in Q_from_P when parameters are unbounded; see https://discourse.pymc.io/t/frequently-asked-questions/74)
-
-# # Mimic loop instantiated by theano.scan for debugging
-# Q_old = 0.499 * np.ones((n_subj))
-# alpha = 0.1 * np.ones((n_subj))
-# for reward, choice in zip(rewards, left_choices):
-#     print("Q_old:", Q_old)
-#     print("Choice:", choice)
-#     print("Reward:", reward)
-#     Q_old = shared.update_Q(reward, choice, Q_old, alpha)
-
-# Starting to think about our hierarchical models:
-# https://discourse.pymc.io/t/filtering-e-g-particle-filter-sequential-mc/117
-# https://gist.github.com/fonnesbeck/342989 (old - prob not useful)
-# https://github.com/hstrey/Hidden-Markov-Models-pymc3/blob/master/Multi-State%20HMM.ipynb (looks better!)
-# https://stackoverflow.com/questions/42146225/problems-building-a-discrete-hmm-in-pymc3
-# https://stackoverflow.com/questions/19875621/hidden-markov-in-pymc3/20288474
+plt.savefig(save_dir + 'compareplot_LOO' + RL_model_id + '.png')
